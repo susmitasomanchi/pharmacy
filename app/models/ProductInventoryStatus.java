e21afc8cf7fc1d1229c385793bc4dbe0eaf385c0
arpit <arpit@arpit-PC>
2014-06-12 16:32:16 +0530

    Batch with inventory

---
+++ app/models/ProductInventoryStatus.java
@@ -0,0 +1,23 @@
+package models;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.avaje.ebean.annotation.EnumValue;
+
+public enum ProductInventoryStatus {
+
+	@EnumValue("AVAILABLE")
+	AVAILABLE,
+	@EnumValue("OUT OF STOCK")
+	OUT_OF_STOCK;
+
+	public static Map<String, String> options() {
+		final LinkedHashMap<String, String> vals = new LinkedHashMap<String, String>();
+		for (final ProductInventoryStatus val : ProductInventoryStatus.values()) {
+			vals.put(val.toString(), val.toString());
+		}
+		return vals;
+	}
+
+}
